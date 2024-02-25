0d77262ea97b5b5c28b0a38f22f88d72e46c2b36
AdamSal2030 <101183222+AdamSal2030@users.noreply.github.com>
2024-02-25 16:52:45 +0500

    forget password changed

--- controllers/userController.js
+++ controllers/userController.js
@@ -45,35 +45,88 @@ const sendResetPasswordMail = async(email,token)=>{
     }
 }
 
-const forget_password = async(req,res)=>{
-    try{
-        const email = req.body.email
-        const password = req.body.password
-        const userData = await User.findOne({email:req.body.email});
+// const forget_password = async(req,res)=>{
+//     try{
+//         const email = req.body.email
+//         const password = req.body.password
+//         const userData = await User.findOne({email:req.body.email});
 
-        if(userData){
-            const updatedUser = await User.findOneAndUpdate(
-                { email },
-                { password, token: randomString },
-                { new: true }
-            );
+//         if(userData){
+//             const updatedUser = await User.findOneAndUpdate(
+//                 { email },
+//                 { password, token: randomString },
+//                 { password},
+//                 { new: true }
+//             );
+//             const forget_password = async(req,res)=>{
+//     try{
+//         const email = req.body.email
+//         const password = req.body.password
+//         const userData = await User.findOne({email:req.body.email});
 
-            const randomString = randomstring.generate();
-            const data = await User.updateOne({email:email},{$set:{token:randomString}})
-            sendResetPasswordMail(userData.email,randomString)
+//         if(userData){
+//             const updatedUser = await User.findOneAndUpdate(
+//                 { email },
+//                 { password, token: randomString },
+//                 { password},
+//                 { new: true }
+                
+//             );
 
-            res.status(200).send({success:true,msg:"bhai email check krle",email})
 
+//             const randomString = randomstring.generate();
+//             const data = await User.updateOne({email:email},{$set:{token:randomString}})
+//             sendResetPasswordMail(userData.email,randomString)
+
+//             res.status(200).send({success:true,msg:"bhai email check krle",email})
+
+//         }
+//         else{
+//             res.status(200).send({success:true,msg:"This email does mot exist. "})
+
+//         }
+
+//     }catch (error) {
+//         res.status(400).send({success:false,msg:error.message})
+//     }
+// }
+
+//             const randomString = randomstring.generate();
+//             const data = await User.updateOne({email:email},{$set:{token:randomString}})
+//             sendResetPasswordMail(userData.email,randomString)
+
+//             res.status(200).send({success:true,msg:"bhai email check krle",email})
+
+//         }
+//         else{
+//             res.status(200).send({success:true,msg:"This email does mot exist. "})
+
+//         }
+
+//     }catch (error) {
+//         res.status(400).send({success:false,msg:error.message})
+//     }
+// }
+
+const forget_password = async (req, res) => {
+    try {
+        const email = req.body.email;
+        const password = req.body.password;
+        const userData = await User.findOne({ email });
+
+        if (userData) {
+            await User.updateOne({ email }, { password });
+
+            
+            return res.status(200).json({ success: true, msg: "Password changed successfully" });
+        } else {
+            return res.status(400).json({ success: false, msg: "User not found" });
         }
-        else{
-            res.status(200).send({success:true,msg:"This email does mot exist. "})
-
-        }
-
-    }catch (error) {
-        res.status(400).send({success:false,msg:error.message})
+    } catch (error) {
+        return res.status(500).json({ success: false, msg: error.message });
     }
-}
+};
+
 
 
 
